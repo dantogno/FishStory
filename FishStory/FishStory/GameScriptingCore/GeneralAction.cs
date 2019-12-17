8236bc3e2998b68f4c946b128ee3a3164305984e
Victor Chelaru <VicChelaru@gmail.com>
2019-12-16 21:01:39 -0700

    Added scripting classes

---
+++ FishStory/FishStory/GameScriptingCore/GeneralAction.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FlatRedBall.Scripting;
+
+namespace FlatRedBall.Scripting
+{
+	public class GeneralAction : IScriptAction
+	{
+		bool hasExecuted = false; 
+		public string Name
+		{
+			get;
+			set;
+		}
+
+		public Func<bool> IsCompleteFunction;
+
+		public Action ActionToPerform;
+
+		public GeneralAction()
+		{
+			IsCompleteFunction = DefaultIsCompleteFunction; 
+		}
+
+		public bool IsComplete()
+		{
+			return IsCompleteFunction();
+		}
+
+		public bool Execute()
+		{
+			ActionToPerform();
+			hasExecuted = true; 
+			return true;
+		}
+
+
+		public override string ToString()
+		{
+			if (!string.IsNullOrEmpty(Name))
+			{
+				return Name;
+			}
+			else
+			{
+				return base.ToString();
+			}
+		}
+
+		private bool DefaultIsCompleteFunction()
+		{
+			return hasExecuted; 
+		}
+	}
+}
---
+++ FishStory/FishStory/GameScriptingCore/IIfScriptEngine.cs
@@ -0,0 +1,10 @@
+﻿namespace FlatRedBall.Scripting
+{
+    public interface IIfScriptEngine
+    {
+        void AndGroup();
+        void OrGroup();
+        void EndGroup();
+        void True();
+    }
+}
