fac4fdefd8cfba36b4bf3cb1b0efcd82d887a452
Jared Bagley <cabrill@gmail.com>
2019-12-16 21:57:44 -0800

    Replaced art with versions that are only 1x.  Made a tilesheet.  Created a test map with tilesheet.

---
+++ FishStory/FishStory/Content/Levels/testonlytilesheet.tsx
@@ -0,0 +1,79 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="master_tiles" tilewidth="16" tileheight="16" tilecount="11097" columns="137">
+ <image source="master_tiles.png" width="2200" height="1296"/>
+ <tile id="824">
+  <properties>
+   <property name="Water" value=""/>
+  </properties>
+ </tile>
+ <tile id="831">
+  <properties>
+   <property name="Water" value=""/>
+  </properties>
+ </tile>
+ <tile id="838">
+  <properties>
+   <property name="Water" value=""/>
+  </properties>
+ </tile>
+ <tile id="1372">
+  <properties>
+   <property name="Water" value=""/>
+  </properties>
+ </tile>
+ <tile id="1379">
+  <properties>
+   <property name="Water" value=""/>
+  </properties>
+ </tile>
+ <tile id="1386">
+  <properties>
+   <property name="Water" value=""/>
+  </properties>
+ </tile>
+ <tile id="1920">
+  <properties>
+   <property name="Water" value=""/>
+  </properties>
+ </tile>
+ <tile id="1927">
+  <properties>
+   <property name="Water" value=""/>
+  </properties>
+ </tile>
+ <tile id="1934">
+  <properties>
+   <property name="Water" value=""/>
+  </properties>
+ </tile>
+ <tile id="3016">
+  <properties>
+   <property name="Water" value=""/>
+  </properties>
+ </tile>
+ <tile id="3023">
+  <properties>
+   <property name="Water" value=""/>
+  </properties>
+ </tile>
+ <tile id="3030">
+  <properties>
+   <property name="Water" value=""/>
+  </properties>
+ </tile>
+ <tile id="3564">
+  <properties>
+   <property name="Water" value=""/>
+  </properties>
+ </tile>
+ <tile id="3571">
+  <properties>
+   <property name="Water" value=""/>
+  </properties>
+ </tile>
+ <tile id="3578">
+  <properties>
+   <property name="Water" value=""/>
+  </properties>
+ </tile>
+</tileset>
