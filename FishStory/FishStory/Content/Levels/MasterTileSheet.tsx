36a6875c2f1feabb7e680e509daef1bd86ab1b51
Jared Bagley <cabrill@gmail.com>
2019-12-23 21:49:03 -0800

    Added squiggly water

--- FishStory/FishStory/Content/Levels/MasterTileSheet.tsx
+++ FishStory/FishStory/Content/Levels/MasterTileSheet.tsx
@@ -1,4 +1,60 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.3.1" name="MasterTileSheet" tilewidth="16" tileheight="16" tilecount="16384" columns="128">
  <image source="MasterTileSheet.png" width="2048" height="2048"/>
+ <tile id="7090">
+  <animation>
+   <frame tileid="7090" duration="100"/>
+   <frame tileid="7092" duration="100"/>
+   <frame tileid="7094" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="7091">
+  <animation>
+   <frame tileid="7091" duration="100"/>
+   <frame tileid="7093" duration="100"/>
+   <frame tileid="7095" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="7218">
+  <animation>
+   <frame tileid="7218" duration="100"/>
+   <frame tileid="7220" duration="100"/>
+   <frame tileid="7222" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="7219">
+  <animation>
+   <frame tileid="7219" duration="100"/>
+   <frame tileid="7221" duration="100"/>
+   <frame tileid="7223" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="7866">
+  <animation>
+   <frame tileid="7866" duration="100"/>
+   <frame tileid="7868" duration="100"/>
+   <frame tileid="7870" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="7867">
+  <animation>
+   <frame tileid="7867" duration="100"/>
+   <frame tileid="7869" duration="100"/>
+   <frame tileid="7871" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="7994">
+  <animation>
+   <frame tileid="7994" duration="100"/>
+   <frame tileid="7996" duration="100"/>
+   <frame tileid="7998" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="7995">
+  <animation>
+   <frame tileid="7995" duration="100"/>
+   <frame tileid="7997" duration="100"/>
+   <frame tileid="7999" duration="100"/>
+  </animation>
+ </tile>
 </tileset>
