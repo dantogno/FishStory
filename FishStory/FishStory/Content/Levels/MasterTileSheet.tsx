194f7221bb21cecc68e6525514b9f6ddade58129
Jared Bagley <cabrill@gmail.com>
2019-12-23 21:26:56 -0800

    Replaced the tile sheet with more finalized, inclusive version

---
+++ FishStory/FishStory/Content/Levels/MasterTileSheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="MasterTileSheet" tilewidth="16" tileheight="16" tilecount="16384" columns="128">
+ <image source="MasterTileSheet.png" width="2048" height="2048"/>
+</tileset>
