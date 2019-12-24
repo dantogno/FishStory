8bb58baa8b4ab4f9589623d1d51c1212df18317c
dantogno <david.antognoli@gmail.com>
2019-12-24 13:31:02 -0500

    Switched from test TestLevel1 to Town for MainLevel, added SolidCollision property
    - added to the top left water tile just to test.

--- FishStory/FishStory/Content/Levels/MasterTileSheet.tsx
+++ FishStory/FishStory/Content/Levels/MasterTileSheet.tsx
@@ -1,6 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.3.1" name="MasterTileSheet" tilewidth="16" tileheight="16" tilecount="16384" columns="128">
  <image source="MasterTileSheet.png" width="2048" height="2048"/>
+ <tile id="0">
+  <properties>
+   <property name="SolidCollision" value="SolidCollision"/>
+  </properties>
+ </tile>
  <tile id="6600">
   <animation>
    <frame tileid="6600" duration="100"/>
