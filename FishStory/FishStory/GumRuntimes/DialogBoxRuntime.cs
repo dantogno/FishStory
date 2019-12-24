8390fb9e5c71f2809cf667203ed309990fd6dcc2
Victor Chelaru <VicChelaru@gmail.com>
2019-12-23 21:41:11 -0700

    Added Gum

---
+++ FishStory/FishStory/GumRuntimes/DialogBoxRuntime.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FishStory.GumRuntimes
+{
+    public partial class DialogBoxRuntime
+    {
+        partial void CustomInitialize () 
+        {
+        }
+    }
+}
