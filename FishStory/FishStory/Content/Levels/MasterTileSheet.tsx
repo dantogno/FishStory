86c1e4e6d557424748f9a312d6ea97fa2bb8e7ce
Victor Chelaru <VicChelaru@gmail.com>
2019-12-28 15:35:20 -0700

    Added water collision

--- FishStory/FishStory/Content/Levels/MasterTileSheet.tsx
+++ FishStory/FishStory/Content/Levels/MasterTileSheet.tsx
@@ -3,10 +3,158 @@
  <image source="MasterTileSheet.png" width="2048" height="2048"/>
  <tile id="0">
   <properties>
-   <property name="SolidCollision" value="SolidCollision"/>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="384">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="386">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="388">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="392">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="394">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="396">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="768">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="770">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="772">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="776">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="778">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="780">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="1152">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="1154">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="1156">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="1160">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="1162">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="1164">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="6578">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="6579">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="6580">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="6581">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="6582">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="6583">
+  <properties>
+   <property name="WaterCollision" value=""/>
   </properties>
  </tile>
  <tile id="6600">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
   <animation>
    <frame tileid="6600" duration="100"/>
    <frame tileid="6856" duration="100"/>
@@ -14,13 +162,49 @@
   </animation>
  </tile>
  <tile id="6601">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
   <animation>
    <frame tileid="6601" duration="100"/>
    <frame tileid="6857" duration="100"/>
    <frame tileid="7113" duration="100"/>
   </animation>
  </tile>
+ <tile id="6706">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="6707">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="6708">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="6709">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="6710">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="6711">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
  <tile id="6728">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
   <animation>
    <frame tileid="6728" duration="100"/>
    <frame tileid="6984" duration="100"/>
@@ -28,11 +212,64 @@
   </animation>
  </tile>
  <tile id="6729">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
   <animation>
    <frame tileid="6729" duration="100"/>
    <frame tileid="6985" duration="100"/>
    <frame tileid="7241" duration="100"/>
   </animation>
+ </tile>
+ <tile id="6834">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="6835">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="6836">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="6837">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="6838">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="6839">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="6856">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="6857">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="6984">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="6985">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
  </tile>
  <tile id="7090">
   <animation>
@@ -48,6 +285,16 @@
    <frame tileid="7095" duration="100"/>
   </animation>
  </tile>
+ <tile id="7112">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="7113">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
  <tile id="7218">
   <animation>
    <frame tileid="7218" duration="100"/>
@@ -62,7 +309,30 @@
    <frame tileid="7223" duration="100"/>
   </animation>
  </tile>
+ <tile id="7240">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="7241">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="7592">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
+ <tile id="7848">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
+ </tile>
  <tile id="7866">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
   <animation>
    <frame tileid="7866" duration="100"/>
    <frame tileid="7868" duration="100"/>
@@ -70,6 +340,9 @@
   </animation>
  </tile>
  <tile id="7867">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
   <animation>
    <frame tileid="7867" duration="100"/>
    <frame tileid="7869" duration="100"/>
@@ -77,6 +350,9 @@
   </animation>
  </tile>
  <tile id="7994">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
   <animation>
    <frame tileid="7994" duration="100"/>
    <frame tileid="7996" duration="100"/>
@@ -84,11 +360,19 @@
   </animation>
  </tile>
  <tile id="7995">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
   <animation>
    <frame tileid="7995" duration="100"/>
    <frame tileid="7997" duration="100"/>
    <frame tileid="7999" duration="100"/>
   </animation>
+ </tile>
+ <tile id="8104">
+  <properties>
+   <property name="WaterCollision" value=""/>
+  </properties>
  </tile>
  <tile id="15360" type="NPC">
   <properties>
