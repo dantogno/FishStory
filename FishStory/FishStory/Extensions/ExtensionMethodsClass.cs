24d7bea888e13e571c169c5c9b3e324c400f3d43
Jared Bagley <cabrill@gmail.com>
2019-12-31 13:47:09 -0800

    Added an extension methods class with an IsNullOrWhitespace method for strings

---
+++ FishStory/FishStory/Extensions/ExtensionMethodsClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishStory
+{
+    public static class ExtensionMethodsClass
+    {
+        public static bool IsNullOrWhitespace(this string stringValue)
+        {
+            return string.IsNullOrWhiteSpace(stringValue);
+        }
+    }
+}
