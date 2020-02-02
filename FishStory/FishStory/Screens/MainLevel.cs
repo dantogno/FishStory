0e09b54e4ea1c029b8f3c7ad404f284b47a562ce
David Antognoli <david.antognoli@gmail.com>
2020-02-02 14:39:44 -0600

    added a debug skip for mayor intro

--- FishStory/FishStory/Screens/MainLevel.cs
+++ FishStory/FishStory/Screens/MainLevel.cs
@@ -366,18 +366,20 @@ namespace FishStory.Screens
             });
             #endregion
             #region Mayor
-            // Mayor
-            // TODO: This is annoying during testing, but turn it back on eventually!
-            If.Check(() => !HasTag("HasSeenWelcomeDialog") && PlayerCharacterInstance.X < 1525);
-            Do.Call(() =>
+            if (!DebuggingVariables.ShouldSkipDay1MayorIntro)
             {
-                SetDialoguePortraitFor(NPCList.FindByName(CharacterNames.Mayor));
-                if (DialogBox.TryShow("WelcomeDialog"))
+                If.Check(() => !HasTag("HasSeenWelcomeDialog") && PlayerCharacterInstance.X < 1525);
+                Do.Call(() =>
                 {
                     SetDialoguePortraitFor(NPCList.FindByName(CharacterNames.Mayor));
-                    PlayerCharacterInstance.ObjectsBlockingInput.Add(DialogBox);
-                }
-            });
+                    if (DialogBox.TryShow("WelcomeDialog"))
+                    {
+                        SetDialoguePortraitFor(NPCList.FindByName(CharacterNames.Mayor));
+                        PlayerCharacterInstance.ObjectsBlockingInput.Add(DialogBox);
+                    }
+                });
+            }
+            
             If.Check(() => HasTag("HasSeenWelcomeDialog"));
             Do.Call(() =>
             {
