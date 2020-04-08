9e89cf4c7fb636d59b93493bbbdd02e6a1873533
Jared Bagley <cabrill@gmail.com>
2020-04-07 22:15:15 -0700

    Added, but not yet implemented a Pause Menu

---
+++ FishStory/FishStory/GumRuntimes/PauseMenuRuntime.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FishStory.GumRuntimes
+{
+    public partial class PauseMenuRuntime
+    {
+        partial void CustomInitialize () 
+        {
+        }
+    }
+}
